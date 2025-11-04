1803301a4061f09fcd8632f73ab7cef077cd3e0f
dodeyk <dodeyk@D361-S01>
2025-11-04 15:47:29 +0100

    Implementation de Designer, Fournir, Mot_Clef, Presenter, Prestation, Specifier

---
+++ USMB_TECH/Models/Fournir.cs
@@ -0,0 +1,8 @@
+﻿namespace USMB_TECH.Models
+{
+    public class Fournir
+    {
+        public int Id_Equipement { get; set; }
+        public int Id_Prestation { get; set; }
+    }
+}
---
+++ USMB_TECH/Models/Prestation.cs
@@ -0,0 +1,17 @@
+﻿namespace USMB_TECH.Models
+{
+    public partial class Prestation
+    {
+        public int Id_Prestation { get; set; }
+        public int Id_Unite_Oeuvre { get; set; }
+        public int Id_Type_Prestation { get; set; }
+        public string Nom_Court{ get; set; }
+        public int Id_Contact { get; set; }
+        public string Intitule_Prestation { get; set; }
+        public string Description_Prestation { get; set; }
+        public double Prix_Revient { get; set; }
+        public double Prix_Vente { get; set; }
+        public bool Peux_Ce_Realiser_Chez_Le_Client { get; set; }
+        public bool Actif { get; set; }
+    }
+}
