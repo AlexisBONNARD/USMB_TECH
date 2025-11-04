23907454ff1e722fb411cc5272541866c92c878e
dodeyk <dodeyk@D361-S01>
2025-11-04 15:34:56 +0100

    Ajout des modeles Unite Oeuvre et Type Prestation

---
+++ USMB_TECH/Models/Unite_Oeuvre.cs
@@ -0,0 +1,8 @@
+﻿namespace USMB_TECH.Models
+{
+    public partial class Unite_Oeuvre
+    {
+        public int Id_Unite_Oeuvre { get; set; }
+        public string Nom_Unite_Oeuvre { get; set; }
+    }
+}
