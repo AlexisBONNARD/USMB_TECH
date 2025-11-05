9abdbcbe994e09de21d327b932ce2b531c7cc10e
dodeyk <dodeyk@D252-S01>
2025-11-05 10:30:55 +0100

    Erreur blazor plus création du dbcontext

---
+++ USMB_TECH/Models/EntityFramework/UsmbTechDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace USMB_TECH.Models.EntityFramework;
+
+public partial class UsmbTechDbContext : DbContext
+{
+    public UsmbTechDbContext()
+    {
+    }
+
+    public UsmbTechDbContext(DbContextOptions<UsmbTechDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Server=51.83.36.122;port=5432;Database=usmbTechDB; uid=s213;password=dN8QKrYi;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- USMB_TECH_Blazor/Program.cs
+++ USMB_TECH_Blazor/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using USMB_TECH_Blazor;
+using USMB_TECH_Blazor.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
