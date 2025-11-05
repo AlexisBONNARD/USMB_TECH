8cbf67d30df50cb64462ed01581c991034619bf8
dodeyk <dodeyk@D252-S01>
2025-11-05 08:52:23 +0100

    Ajout de la table Thematique

---
+++ USMB_TECH/Models/Thematique.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace USMB_TECH.Models
+{
+    [Table("thematique")]
+    public class Thematique
+    {
+        [Key]
+        [Column("id_thematique")]
+        public int Id_Thematique { get; set; }
+
+        [Column("id_sous_thematique")]
+        public int Id_Sous_Thematique { get; set; }
+
+        [Column("nom_thematique")]
+        [MaxLength(50)]
+        public string Nom_Thematique { get; set; }
+        }
+}
