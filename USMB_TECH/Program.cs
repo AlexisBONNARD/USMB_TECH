bd51ce9e8d4834423f93e7c3793e1850814429c3
barryt <barryt@D350-S14>
2025-11-07 16:05:44 +0100

    Edit program.cs

--- USMB_TECH/Program.cs
+++ USMB_TECH/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using System;
 using USMB_TECH.Models;
 using USMB_TECH.Models.EntityFramework;
 using USMB_TECH.Models.Repository;
@@ -10,13 +12,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<UsmbTechDbContext>();
 builder.Services.AddScoped<IMainRepository<Laboratoire, string>, LaboratoireManager>();
 builder.Services.AddScoped<IMainRepository<Plateforme, int>, PlateformeManager>();
 builder.Services.AddScoped<IMainRepository<Thematique, int>, ThematiqueManager>();
 builder.Services.AddScoped<IMainRepository<Prestation, int>, PrestationManager>();
 builder.Services.AddScoped<IMainRepository<Contact_USMB, int>, Contact_USMBManager>();
 builder.Services.AddScoped<IMainRepository<Equipement, int>, EquipementManager>();
+builder.Services.AddDbContext<UsmbTechDbContext>(options => options.UseNpgsql(
+    builder.Configuration.GetConnectionString("UsmbTechDbContext")));
+
+//builder.Services.AddAutoMapper(cfg =>
+//{
+
+
+//});
 
 var app = builder.Build();
 
