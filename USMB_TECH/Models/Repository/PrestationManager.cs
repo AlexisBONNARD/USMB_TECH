dcb66cf205d1949b23ac8394d455149172a65772
droueest <esteban.drouet@etu.univ-smb.fr>
2025-11-07 13:46:54 +0100

    Add Manager(Prestation and Thematique)

---
+++ USMB_TECH/Models/Repository/PrestationManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using USMB_TECH.Models.EntityFramework;
+
+namespace USMB_TECH.Models.Repository
+{
+    public class PrestationManager : IMainRepository<Prestation, int>
+    {
+        public UsmbTechDbContext context = new UsmbTechDbContext();
+
+        public async Task<IEnumerable<Prestation>> GetAllAsync() 
+        {
+            return await context.Prestations.ToListAsync();
+        }
+
+        public async Task<Prestation?> GetByIdAsync(int id) 
+        {
+            return await context.Prestations.FindAsync(id);
+        }
+
+        public async Task AddAsync(Prestation entity) 
+        {
+             context.Prestations.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Prestation entityToUpdate, Prestation entity) 
+        {
+            context.Prestations.Attach(entityToUpdate);
+            context.Prestations.Entry(entityToUpdate).CurrentValues.SetValues(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Prestation entity) 
+        {
+            context.Prestations.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ USMB_TECH/Models/Repository/ThematiqueManager.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using USMB_TECH.Models.EntityFramework;
+
+namespace USMB_TECH.Models.Repository
+{
+    public class ThematiqueManager : IMainRepository<Thematique, int>
+    {
+        public UsmbTechDbContext context = new UsmbTechDbContext();
+
+        public async Task<IEnumerable<Thematique>> GetAllAsync()
+        {
+            return await context.Thematiques.ToListAsync();
+        }
+
+        public async Task<Thematique?> GetByIdAsync(int id)
+        {
+            return await context.Thematiques.FindAsync(id);
+        }
+
+        public async Task AddAsync(Thematique entity) 
+        {
+            context.Thematiques.Add(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Thematique entityToUpdate, Thematique entity) 
+        {
+            context.Thematiques.Attach(entityToUpdate);
+            context.Thematiques.Entry(entityToUpdate).CurrentValues.SetValues(entity);
+            await context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(Thematique entity) 
+        {
+            context.Thematiques.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
