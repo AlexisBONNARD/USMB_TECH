6e3f99e398438061f1e3ae42cb328f36d88b494d
AlexisBONNARD <alexis.bonnard@etu.univ-smb.fr>
2025-11-07 14:37:58 +0100

    update : PlateformeDTO

--- USMB_TECH/DTO/PlateformeDTO.cs
+++ USMB_TECH/DTO/PlateformeDTO.cs
@@ -2,20 +2,27 @@
 {
     public class PlateformeDTO
     {
-        public int Id { get; set; }
-
-        public string? Name { get; set; }
+        public int Id_Plateforme { get; set; }
+        public string Nom_Plateforme { get; set; }
+        public string Description_Plateforme { get; set; }
+        public string Nom_Contenu { get; set; }
+        public string Url_Contenu { get; set; }
+        public string Description_Contenu { get; set; }
 
         public override bool Equals(object? obj)
         {
-            return obj is PlateformeDTO dTO &&
-                   Id == dTO.Id &&
-                   Name == dTO.Name;
+            return obj is PlateformeDTO DTO &&
+                   Id_Plateforme == DTO.Id_Plateforme &&
+                   Nom_Plateforme == DTO.Nom_Plateforme &&
+                   Description_Plateforme == DTO.Description_Plateforme &&
+                   Nom_Contenu == DTO.Nom_Contenu &&
+                   Url_Contenu == DTO.Url_Contenu &&
+                   Description_Contenu == DTO.Description_Contenu;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(Id, Name);
+            return HashCode.Combine(Id_Plateforme, Nom_Plateforme, Description_Plateforme, Nom_Contenu, Url_Contenu, Description_Contenu);
         }
     }
 }
