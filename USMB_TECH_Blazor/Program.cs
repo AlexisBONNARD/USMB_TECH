6cb95f742fad7a9f2e8073b5b9fe51b3fd66888d
droueest <esteban.drouet@etu.univ-smb.fr>
2025-11-10 08:44:44 +0100

    Add : String in Program.cs to allow WebService to call every classes

--- USMB_TECH_Blazor/Program.cs
+++ USMB_TECH_Blazor/Program.cs
@@ -11,10 +11,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped<IMainService<Equipement, int>>(eq => new WebService<Equipement, int>());
-builder.Services.AddScoped<IMainService<Laboratoire, string>>(eq => new WebService<Laboratoire, string>());
-builder.Services.AddScoped<IMainService<Plateforme, int>>(eq => new WebService<Plateforme, int>());
-builder.Services.AddScoped<IMainService<Prestation, int>>(eq => new WebService<Prestation, int>());
-builder.Services.AddScoped<IMainService<Thematique, int>>(eq => new WebService<Thematique, int>());
+builder.Services.AddScoped<IMainService<Equipement, int>>(eq => new WebService<Equipement, int>("Equipements"));
+builder.Services.AddScoped<IMainService<Laboratoire, string>>(eq => new WebService<Laboratoire, string>("Laboratoires"));
+builder.Services.AddScoped<IMainService<Plateforme, int>>(eq => new WebService<Plateforme, int>("Plateformes"));
+builder.Services.AddScoped<IMainService<Prestation, int>>(eq => new WebService<Prestation, int>("Prestations"));
+builder.Services.AddScoped<IMainService<Thematique, int>>(eq => new WebService<Thematique, int>("Thematiques"));
 
 await builder.Build().RunAsync();
--- USMB_TECH_Blazor/Service/WebService.cs
+++ USMB_TECH_Blazor/Service/WebService.cs
@@ -6,30 +6,32 @@ namespace USMB_TECH_Blazor.Service
     public class WebService<TEntity, TIdentity>: IMainService<TEntity,TIdentity>
     {
         private readonly HttpClient _httpClient;
-        public WebService() 
+        private string _endpoint;
+        public WebService(string endpoint) 
         {
             _httpClient = new HttpClient
             {
-                BaseAddress = new Uri("https://localhost:7093/")
+                BaseAddress = new Uri("https://localhost:7093/api/")
             };
+            this._endpoint = endpoint;
         }
         public async Task AddAsync(TEntity entity) 
         {
-            await _httpClient.PostAsJsonAsync($"", entity);
+            await _httpClient.PostAsJsonAsync($"{_endpoint}", entity);
         }
 
         public async Task DeleteAsync(TIdentity id) 
         {
-            await _httpClient.DeleteAsync($"");
+            await _httpClient.DeleteAsync($"{_endpoint}/{id}");
         }
 
         public async Task<IEnumerable<TEntity>> GetAllAsync() 
         {
-            return await _httpClient.GetFromJsonAsync<IEnumerable<TEntity>>($"");
+            return await _httpClient.GetFromJsonAsync<IEnumerable<TEntity>>($"{_endpoint}");
         }
         public async Task<TEntity?> GetByIdAsync(TIdentity id) 
         {
-            return await _httpClient.GetFromJsonAsync<TEntity?>($"");
+            return await _httpClient.GetFromJsonAsync<TEntity?>($"{_endpoint}/{id}");
         }
 
         public async Task UpdateAsync(TEntity updatedEntity) 
@@ -37,7 +39,7 @@ namespace USMB_TECH_Blazor.Service
             PropertyInfo propertyInfo = typeof(TEntity).GetProperty("Id");
             if(propertyInfo != null) throw new InvalidOperationException("L'entité ne possède pas de propriété Id");
             var id = propertyInfo.GetValue(updatedEntity);
-            await _httpClient.PutAsJsonAsync($"", updatedEntity);
+            await _httpClient.PutAsJsonAsync($"{_endpoint}/{id}", updatedEntity);
         }
     }
 }
