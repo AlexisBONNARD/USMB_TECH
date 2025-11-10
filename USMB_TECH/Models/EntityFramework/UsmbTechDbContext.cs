00b9a4072e2ac24008260b3eab15add821b96a89
TitouanB2 <titouan.barry@etu.univ-smb.fr>
2025-11-10 16:10:47 +0100

    Modification db context pour plus de rapidite

--- USMB_TECH/Models/EntityFramework/UsmbTechDbContext.cs
+++ USMB_TECH/Models/EntityFramework/UsmbTechDbContext.cs
@@ -49,18 +49,11 @@ public partial class UsmbTechDbContext : DbContext
         : base(options)
     {
     }
+
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-
-        optionsBuilder
-            .UseLazyLoadingProxies()
-            .UseNpgsql("Server=51.83.36.122;port=5432;Database=usmbTechDB; uid=s213;password=dN8QKrYi;");
-    }
-
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseNpgsql("Server=51.83.36.122;port=5432;Database=usmbTechDB; uid=s213;password=dN8QKrYi;");
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Server=51.83.36.122;port=5432;Database=usmbTechDB; uid=s213;password=dN8QKrYi;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
