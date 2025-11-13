232938aa40439af5a72282021d1d6102f324b269
dodeyk <dodeyk@D101-S01>
2025-11-13 08:11:23 +0100

    Reparation d'un probleme dans equipement

--- USMB_TECH_Blazor/Models/Equipement.cs
+++ USMB_TECH_Blazor/Models/Equipement.cs
@@ -35,6 +35,6 @@ namespace USMB_TECH_Blazor.Models
 
         public bool Actif { get; set; }
 
-        //public virtual ICollection<Exemple_Utilisation> Exemple_Utilisations { get; set; } = new List<Exemple_Utilisation>();
+        public virtual ICollection<Exemple_Utilisation> Exemple_Utilisations { get; set; } = new List<Exemple_Utilisation>();
     }
 }
