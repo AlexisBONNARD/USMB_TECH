78f9178c9e0da517f7e1f5ae5fe368874744e726
TitouanB2 <titouan.barry@etu.univ-smb.fr>
2025-11-14 14:23:51 +0100

    Ajout relation entre Plateforme et mot clef

--- USMB_TECH/Models/Repository/PlateformeManager.cs
+++ USMB_TECH/Models/Repository/PlateformeManager.cs
@@ -14,8 +14,12 @@ namespace USMB_TECH.Models.Repository
         }
         public async Task<Plateforme?> GetByIdAsync(int id)
         {
-            return await context.Plateformes.FindAsync(id);
+            return await context.Plateformes
+                .Include(p => p.Specifiers)
+                    .ThenInclude(s => s.Mot_ClefNavigation)
+                .FirstOrDefaultAsync(p => p.Id_Plateforme == id);
         }
+
         public async Task UpdateAsync(Plateforme entityToUpdate, Plateforme entity)
         {
             context.Plateformes.Attach(entityToUpdate);
