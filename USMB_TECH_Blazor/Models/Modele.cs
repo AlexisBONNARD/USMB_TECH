22e60b26c13edcf185f9d9b83d926b0581ddd789
droueest <esteban.drouet@etu.univ-smb.fr>
2025-11-21 17:15:02 +0100

    Add : Marque from AddEquipement is working

--- USMB_TECH_Blazor/Models/Modele.cs
+++ USMB_TECH_Blazor/Models/Modele.cs
@@ -2,26 +2,15 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace USMB_TECH_Blazor.Models
-{
-    [Table("modele")]   
-    public partial class Modele
+{ 
+    public class Modele
     {
-        [Key]
-        [Column("id_modele")]
         public int Id_Modele { get; set; }
 
-        [Column("id_marque")]
         public int Id_Marque { get; set; }
-
-        [Column("nom_modele")]
-        [MaxLength(50)] 
         public string Nom_Modele { get; set; }
 
-        [InverseProperty(nameof(Equipement.ModeleNavigation))]
         public virtual ICollection<Equipement> Equipements { get; set; } = new List<Equipement>();
-
-        [ForeignKey("Id_Marque")]
-        [InverseProperty(nameof(Marque.Modeles))]
         public virtual Marque? MarqueNavigation { get; set; } = null!;
     }
 }
