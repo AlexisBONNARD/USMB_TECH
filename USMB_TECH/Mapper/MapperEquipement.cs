406a7711bb82e1bc7e94a9de9f608fe3d00b6309
dodeyk <dodeyk@D361-S01>
2025-11-24 17:01:55 +0100

    possibilite d'ajouter une photo lors de la creation de l'equipement

--- USMB_TECH/Mapper/MapperEquipement.cs
+++ USMB_TECH/Mapper/MapperEquipement.cs
@@ -8,21 +8,29 @@ namespace USMB_TECH.Mapper
         public MapperEquipement()
         {
             CreateMap<EquipementAddDTO, Equipement>()
-            .ForMember(dest => dest.PlateformeNavigation,
-               opt => opt.MapFrom(src => new Plateforme { Nom_Plateforme = src.Nom_Plateforme }))
-            .ForMember(dest => dest.Type_EquipementNavigation,
-               opt => opt.MapFrom(src => new Type_Equipement { Nom_Type = src.Type_Equipement }))
-            .ForMember(dest => dest.Id_Plateforme, opt => opt.Ignore())
-            .ForMember(dest => dest.Id_Type_Equipement, opt => opt.Ignore())
-            .ForMember(dest => dest.ModeleNavigation, 
-            opt => opt.MapFrom(src => new Modele
-            { Nom_Modele = src.Nom_Modele, 
-                MarqueNavigation = new Marque
-                {
-                    Nom_Marque = src.Nom_Marque
-                }
-            }))
-            .ForMember(dest => dest.Id_Modele, opt => opt.Ignore());
+                .ForMember(dest => dest.PlateformeNavigation,
+                    opt => opt.MapFrom(src => new Plateforme { Nom_Plateforme = src.Nom_Plateforme }))
+                .ForMember(dest => dest.Type_EquipementNavigation,
+                    opt => opt.MapFrom(src => new Type_Equipement { Nom_Type = src.Type_Equipement }))
+                .ForMember(dest => dest.Id_Plateforme, opt => opt.Ignore())
+                .ForMember(dest => dest.Id_Type_Equipement, opt => opt.Ignore())
+                .ForMember(dest => dest.ModeleNavigation,
+                    opt => opt.MapFrom(src => new Modele
+                    {
+                        Nom_Modele = src.Nom_Modele,
+                        MarqueNavigation = new Marque
+                        {
+                            Nom_Marque = src.Nom_Marque
+                        }
+                    }))
+                .ForMember(dest => dest.Id_Modele, opt => opt.Ignore())
+                .ForMember(dest => dest.Photos,
+                    opt => opt.MapFrom(src => src.Photos.Select(p => new Photo
+                    {
+                        Nom_Photo = p.Nom_Photo,
+                        Url_Photo = p.Url_Photo
+                    }).ToList()
+                ));
         }
     }
 }
