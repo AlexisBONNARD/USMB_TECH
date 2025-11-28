453d6fd7de99f0b651a87e45ffcd5b266e6f670e
TitouanB2 <titouan.barry@etu.univ-smb.fr>
2025-11-28 09:29:26 +0100

    essaie fix erreur exemple utilisation update equipement + correction contrainte exclusivité exemple utilisation

--- USMB_TECH/Mapper/MapperEquipement.cs
+++ USMB_TECH/Mapper/MapperEquipement.cs
@@ -57,36 +57,47 @@ namespace USMB_TECH.Mapper
 
 
             CreateMap<UpdateEquipementDto, Equipement>()
-                            .ForMember(dest => dest.Id_Equipement, opt => opt.MapFrom(src => src.Id_Equipement))
-                            .ForMember(dest => dest.Id_Plateforme, opt => opt.MapFrom(src => src.Id_Plateforme))
-                            .ForMember(dest => dest.Id_Modele, opt => opt.MapFrom(src => src.Id_Modele))
-                            .ForMember(dest => dest.Id_Type_Equipement, opt => opt.MapFrom(src => src.Id_Type_Equipement))
-                            .ForMember(dest => dest.Nom_Equipement, opt => opt.MapFrom(src => src.Nom_Equipement))
-                            .ForMember(dest => dest.Num_Immobilisation, opt => opt.MapFrom(src => src.Num_Immobilisation))
-                            .ForMember(dest => dest.Date_Acquisition, opt => opt.MapFrom(src => src.Date_Acquisition))
-                            .ForMember(dest => dest.Prix_Achat, opt => opt.MapFrom(src => (double)src.Prix_Achat))
-                            .ForMember(dest => dest.Prix_Revient, opt => opt.MapFrom(src => (double)src.Prix_Revient))
-                            .ForMember(dest => dest.Description_Technique, opt => opt.MapFrom(src => src.Description_Technique))
-                            .ForMember(dest => dest.Disponibilite, opt => opt.MapFrom(src => src.Disponibilite))
-                            .ForMember(dest => dest.Autonomie, opt => opt.MapFrom(src => src.Autonomie))
-                            .ForMember(dest => dest.Utilisable_Chez_Le_Client, opt => opt.MapFrom(src => src.Utilisable_Chez_Le_Client))
-                            .ForMember(dest => dest.Actif, opt => opt.MapFrom(src => src.Actif))
+                .ForMember(dest => dest.Id_Equipement, opt => opt.MapFrom(src => src.Id_Equipement))
+                .ForMember(dest => dest.Id_Plateforme, opt => opt.MapFrom(src => src.Id_Plateforme))
+                .ForMember(dest => dest.Id_Modele, opt => opt.MapFrom(src => src.Id_Modele))
+                .ForMember(dest => dest.Id_Type_Equipement, opt => opt.MapFrom(src => src.Id_Type_Equipement))
+                .ForMember(dest => dest.Nom_Equipement, opt => opt.MapFrom(src => src.Nom_Equipement))
+                .ForMember(dest => dest.Num_Immobilisation, opt => opt.MapFrom(src => src.Num_Immobilisation))
+                .ForMember(dest => dest.Date_Acquisition, opt => opt.MapFrom(src => src.Date_Acquisition))
+                .ForMember(dest => dest.Prix_Achat, opt => opt.MapFrom(src => (double)src.Prix_Achat))
+                .ForMember(dest => dest.Prix_Revient, opt => opt.MapFrom(src => (double)src.Prix_Revient))
+                .ForMember(dest => dest.Description_Technique, opt => opt.MapFrom(src => src.Description_Technique))
+                .ForMember(dest => dest.Disponibilite, opt => opt.MapFrom(src => src.Disponibilite))
+                .ForMember(dest => dest.Autonomie, opt => opt.MapFrom(src => src.Autonomie))
+                .ForMember(dest => dest.Utilisable_Chez_Le_Client, opt => opt.MapFrom(src => src.Utilisable_Chez_Le_Client))
+                .ForMember(dest => dest.Actif, opt => opt.MapFrom(src => src.Actif))
 
-                            // ⚡ Photos corrigées pour respecter la contrainte
-                            .ForMember(dest => dest.Photos, opt => opt.MapFrom(src =>
-                                src.Photos.Select(p => new Photo
-                                {
-                                    Id_Photo = p.Id_Photo,
-                                    Nom_Photo = p.Nom_Photo,
-                                    Url_Photo = p.Url_Photo,
-                                    Id_Equipement = src.Id_Equipement, // ⚡ obligatoire
-                                    Id_Plateforme = null               // ⚡ forcé à null
-                                }).ToList()
-                            ))
+                // ⚡ ExempleUtilisations correctement mappés
+                .ForMember(dest => dest.Exemple_Utilisations, opt => opt.MapFrom(src =>
+                    src.ExempleUtilisations.Select(eu => new Exemple_Utilisation
+                    {
+                        Id_Exemple_Utilisation = eu.Id_Exemple_Utilisation,
+                        Nom_Utilisation = eu.Nom_Utilisation,
+                        Description_Utilisation = eu.Description_Utilisation,
+                        Id_Equipement = src.Id_Equipement
+                    }).ToList()
+                ))
 
-                            // ⚡ Collections : on les ignore si tu veux éviter les suppressions involontaires
-                            .ForMember(dest => dest.Exemple_Utilisations, opt => opt.Ignore())
-                            .ForMember(dest => dest.Fournirs, opt => opt.Ignore());
+                // ⚡ Photos corrigées pour respecter la contrainte
+                .ForMember(dest => dest.Photos, opt => opt.MapFrom(src =>
+                    src.Photos.Select(p => new Photo
+                    {
+                        Id_Photo = p.Id_Photo,
+                        Nom_Photo = p.Nom_Photo,
+                        Url_Photo = p.Url_Photo,
+                        Id_Equipement = src.Id_Equipement,
+                        Id_Plateforme = null
+                    }).ToList()
+                ))
+
+
+                .ForMember(dest => dest.Fournirs, opt => opt.Ignore());
+
 
 
 
