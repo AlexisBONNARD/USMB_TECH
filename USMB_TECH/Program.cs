eaa669908d87d623b7dd7c879bbf9de78991699e
barryt <barryt@D350-S14>
2025-11-28 15:43:00 +0100

    rajout https://

--- USMB_TECH/Program.cs
+++ USMB_TECH/Program.cs
@@ -37,7 +37,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowBlazor",
         policy =>
         {
-            policy.WithOrigins("blazor-usmbtech-ekf6gkgretedd7bc.francecentral-01.azurewebsites.net")
+            policy.WithOrigins("https://blazor-usmbtech-ekf6gkgretedd7bc.francecentral-01.azurewebsites.net")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
