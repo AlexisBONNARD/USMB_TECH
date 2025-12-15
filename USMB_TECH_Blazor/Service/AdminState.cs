c3cb65f99e933714d2c2e83e6347697c58b7c4df
AlexisBONNARD <alexis.bonnard@etu.univ-smb.fr>
2025-12-15 11:02:42 +0100

    refactor : plateformes on pole d'expertise refactor : contact on lab, move under presta refactor : all data on prise contact refactor : on contact prénom was with accent add: connection admin and these hidden gifts

---
+++ USMB_TECH_Blazor/Service/AdminState.cs
@@ -0,0 +1,27 @@
+﻿namespace USMB_TECH_Blazor.Service
+{
+    public class AdminState
+    {
+        private const string StorageKey = "isAdmin";
+
+        public bool IsAdmin { get; private set; }
+
+        public event Action? OnChange;
+
+        public void SetAdmin(bool value)
+        {
+            IsAdmin = value;
+            NotifyStateChanged();
+        }
+
+        public void EnableAdmin()
+            => SetAdmin(true);
+
+        public void DisableAdmin()
+            => SetAdmin(false);
+
+        private void NotifyStateChanged()
+            => OnChange?.Invoke();
+    }
+
+}
