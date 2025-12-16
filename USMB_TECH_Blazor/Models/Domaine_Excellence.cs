088137d7b486d80c632a8e656fbf9d5f48d74ecb
k-nny <kenny.dodey@etu.univ-smb.fr>
2025-12-16 16:34:18 +0100

    ajout de la photo fonctionnel dans ajouter domaine

--- USMB_TECH_Blazor/Models/Domaine_Excellence.cs
+++ USMB_TECH_Blazor/Models/Domaine_Excellence.cs
@@ -1,9 +1,13 @@
-﻿namespace USMB_TECH_Blazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace USMB_TECH_Blazor.Models
 {
     public class Domaine_Excellence
     {
         public int Id_Domaine_Excellence { get; set; }
+        [Required(ErrorMessage = "L'intitulé du domaine d'excellence est obligatoire.")]
         public string? Intitule_Domaine_Excellence { get; set; }
+        [Required(ErrorMessage = "La description du domaine d'excellence est obligatoire.")]
         public string? Description_Domaine_Excellence { get; set; }
         public virtual ICollection<Photo> Photos { get; set; } = new List<Photo>();
         public virtual ICollection<Pole_Expertise> Pole_Expertises { get; set; } = new List<Pole_Expertise>();
