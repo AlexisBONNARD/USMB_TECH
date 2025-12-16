a842835cf5a4b334f554fa94a41a7cba4040afb9
bensaim claire <claire.bensaim@etu.univ-smb.fr>
2025-12-16 15:17:16 +0100

    nouvelle migration

---
+++ USMB_TECH/Migrations/20251216141525_updateLengthNumSecuSocial.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace USMB_TECH.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateLengthNumSecuSocial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "num_securite_social",
+                schema: "usmbTech",
+                table: "contact_usmb",
+                type: "character varying(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(13)",
+                oldMaxLength: 13);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "num_securite_social",
+                schema: "usmbTech",
+                table: "contact_usmb",
+                type: "character varying(13)",
+                maxLength: 13,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(15)",
+                oldMaxLength: 15);
+        }
+    }
+}
