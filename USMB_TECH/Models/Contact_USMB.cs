c6d15d219b722ffbd881e1cf8f9f64c32560e0fe
bensaim claire <claire.bensaim@etu.univ-smb.fr>
2025-12-16 15:14:09 +0100

    ajout dun contact en meme temps qu'un labo

--- USMB_TECH/Models/Contact_USMB.cs
+++ USMB_TECH/Models/Contact_USMB.cs
@@ -23,7 +23,7 @@ namespace USMB_TECH.Models
         public string Code_RH { get; set; }
 
         [Column("num_securite_social")]
-        [StringLength(13)]
+        [StringLength(15)]
         public string Num_Securite_Social { get; set; }
 
         [Column("nom_contact")]
