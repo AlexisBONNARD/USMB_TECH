f451dbe487d314f64ba056cbd7a6da509bbe0dc0
JulesCamelin <jules.camelin@etu.univ-smb.fr>
2026-01-05 15:30:17 +0100

    Ajout de la solution des tests

---
+++ USMB_TECHTests/Controllers/Type_EquipementsControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using USMB_TECH.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USMB_TECH.Controllers.Tests
+{
+    [TestClass()]
+    public class Type_EquipementsControllerTests
+    {
+        [TestMethod()]
+        public void GetType_EquipementsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
