87aaebe297f229ecb88748cd2c5d5bdf574edd60
Kenny Dodey <kenny.dodey@sistema-strategy.com>
2026-01-14 18:53:31 +0100

    potentiel retour de la recherche (sans ia)

--- USMB_TECH_Blazor/Program.cs
+++ USMB_TECH_Blazor/Program.cs
@@ -12,8 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<AdminState>();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddScoped<IMainService<Equipement, int>>(eq => new WebService<Equipement, int>("Equipements"));
 builder.Services.AddScoped<IMainService<Laboratoire, string>>(eq => new WebService<Laboratoire, string>("Laboratoires"));
 builder.Services.AddScoped<IMainService<LaboratoireUpdate, string>>(eq => new WebService<LaboratoireUpdate, string>("Laboratoires"));
