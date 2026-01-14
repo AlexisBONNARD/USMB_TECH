cefa2dda6e5fde4ed3b4e3e22d2a839265416aac
k-nny <kenny.dodey@etu.univ-smb.fr>
2026-01-14 09:08:47 +0100

    ajout de la bonne adresse url

--- USMB_TECH/Program.cs
+++ USMB_TECH/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IMainRepository<Laboratoire, string>, LaboratoireManager>();
 builder.Services.AddScoped<IMainRepository<Pole_Expertise, int>, Pole_ExpertiseManager>();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7093/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("api-usmbtech-hvevdvgbdwh7aqf5.francecentral-01.azurewebsites.net/") });
 builder.Services.AddScoped<IMainRepository<Thematique, int>, ThematiqueManager>();
 builder.Services.AddScoped<IMainRepository<Prise_Contact, int>, Prise_ContactManager>();
 builder.Services.AddScoped<IMainRepository<Prestation, int>, PrestationManager>();
@@ -49,7 +49,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowBlazor",
         policy =>
         {
-            policy.WithOrigins("https://localhost:7264")
+            policy.WithOrigins("https://blazor-usmbtech-ekf6gkgretedd7bc.francecentral-01.azurewebsites.net/")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
