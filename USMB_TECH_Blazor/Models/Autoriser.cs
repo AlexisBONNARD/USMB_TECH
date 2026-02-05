d61fce23368667d41d342d40890659d233b8f1eb
AlexisBONNARD <alexis.bonnard@etu.univ-smb.fr>
2026-02-05 16:48:37 +0100

    equipement

---
+++ USMB_TECH_Blazor/Models/Autoriser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace USMB_TECH_Blazor.Models
+{
+    public class Autoriser
+    {
+        public int Id_Equipement { get; set; }
+
+        public int Id_Type_Client { get; set; }
+
+        public virtual Equipement? EquipementNavigation { get; set; } = null!;
+
+        public virtual Type_Client? Type_ClientNavigation { get; set; } = null!;
+    }
+}
---
+++ USMB_TECH_Blazor/Models/Type_Client.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace USMB_TECH_Blazor.Models
+{
+    public class Type_Client
+    {
+        public int Id_Type_Client { get; set; }
+
+        public string Nom_Type_Client { get; set; }
+
+        public double Mult_Tarif_Type_Client { get; set; }
+
+        public virtual ICollection<Prise_Contact> Prise_Contacts { get; set; } = new List<Prise_Contact>();
+
+        public virtual ICollection<Autoriser> Autorisers { get; set; } = new List<Autoriser>();
+    }
+
+    public class TypeClientSelection
+    {
+        public int Id_Type_Client { get; set; }
+        public string Nom_Type_Client { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
